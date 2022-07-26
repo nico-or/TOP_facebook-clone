ad71ecc2b0b3e25716caa4c48b518cafca58c7ad
Nicolás Órdenes <nicordenes@gmail.com>
2022-07-25 23:00:00 -0400

    Add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create([
+              { email: 'foo@example.com', password: '123456' },
+              { email: 'bar@example.com', password: '123456' }
+            ])

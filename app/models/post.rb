9d6ad9965ed81fd8a4386ce6e42c8f992f1b4e00
Nicolás Órdenes <nicordenes@gmail.com>
2022-07-30 17:02:54 -0400

    Add Like model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
 
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liking_users, through: :likes, source: :user
 end

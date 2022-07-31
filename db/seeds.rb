3786885d66efb49a8b65d590147179e296ed4470
Nicolás Órdenes <nicordenes@gmail.com>
2022-07-31 00:42:59 -0400

    Add Faker gem
    To seed the database with better fixtures.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create([
-              { email: 'foo@example.com', password: '123456' },
-              { email: 'bar@example.com', password: '123456' }
-            ])
+User.create(email: 'user@example.com', password: '123456')
+
+7.times do
+  user = Faker::Internet.user(:safe_email, :password)
+  User.create(email: user[:safe_email], password: user[:password])
+end
+
+users = User.all.records
+5.times do
+  user = users.sample
+  text = Faker::Lorem.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 6)
+  user.posts.create(body: text)
+end

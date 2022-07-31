bf9e18e98586d9f2fc51c3f075ebbef119a384df
Nicolás Órdenes <nicordenes@gmail.com>
2022-07-30 22:47:52 -0400

    Refactor likes controller test

--- test/controllers/likes_controller_test.rb
+++ test/controllers/likes_controller_test.rb
@@ -1,20 +1,21 @@
 require "test_helper"
 
 class LikesControllerTest < ActionDispatch::IntegrationTest
-  test "should create a new like" do
-    sign_in users(:one)
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
 
-    user = users(:one)
+  test "should create a like" do
     post = posts(:two)
 
     assert_difference("Like.count") do
-      post likes_url, params: { like: { user_id: user.id, post_id: post.id } }
+      post likes_url, params: { like:
+          { user_id: @user.id, post_id: post.id } }
     end
   end
 
-  test "should delete an existing like" do
-    sign_in users(:one)
-
+  test "should destroy like" do
     like = likes(:one)
 
     assert_difference("Like.count", -1) do
@@ -29,20 +30,16 @@ class LikesControllerTest < ActionDispatch::IntegrationTest
       ERROR: duplicate key value violates unique constraint "index_likes_on_post_id_and_user_id"
     DOC
 
-    sign_in users(:two)
-
-    user = users(:one)
     post = posts(:two)
 
     assert_no_difference("Like.count") do
-      post likes_url, params: { like: { user_id: user.id, post_id: post.id } }
+      post likes_url, params: { like:
+        { user_id: @user.id, post_id: post.id } }
     end
   end
 
   test "should not delete post of other users" do
     skip "Fails with error: ActiveRecord::RecordNotFound: Couldn't find Like with..."
-
-    sign_in users(:one)
 
     like = likes(:two)
 

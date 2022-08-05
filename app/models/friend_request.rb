04d678397aa781f72b00d5342e267ea01f872d40
Nicolás Órdenes <nicordenes@gmail.com>
2022-08-05 10:57:20 -0400

    Add status enum to FriendRequest model

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,4 +1,6 @@
 class FriendRequest < ApplicationRecord
   belongs_to :sender, class_name: :User
   belongs_to :receiver, class_name: :User
+
+  enum :status, [:pending, :accepted, :rejected]
 end
--- test/models/friend_request_test.rb
+++ test/models/friend_request_test.rb
@@ -9,4 +9,12 @@ class FriendRequestTest < ActiveSupport::TestCase
     assert users(:one).sent_friend_requests.include? req
     assert users(:two).received_friend_requests.include? req
   end
+
+  test "default status is pending" do
+    request = FriendRequest.create(
+      sender: users(:one),
+      receiver: users(:two))
+
+    assert_equal request.pending?, true
+  end
 end

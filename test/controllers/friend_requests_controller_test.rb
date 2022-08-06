4163bbbef2c5dcd7aa5dc19be396394f6bf6ebbc
Nicolás Órdenes <nicordenes@gmail.com>
2022-08-06 13:07:47 -0400

    Refactor friend_requests_controller_test
    Remove duplication and dependcy of fixture file.

--- test/controllers/friend_requests_controller_test.rb
+++ test/controllers/friend_requests_controller_test.rb
@@ -4,6 +4,10 @@ class FriendRequestsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
     sign_in @user
+
+    @request = FriendRequest.create(
+      sender: users(:one),
+      receiver: users(:two))
   end
 
   test "should create a FriendRequest" do
@@ -13,23 +17,18 @@ class FriendRequestsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should destroy a FriendRequest" do
-    request = friend_requests(:one)
-
     assert_difference("FriendRequest.count", -1) do
-      delete friend_request_url(request)
+      delete friend_request_url(@request)
     end
   end
 
   test "should accept a pending FriendRequest" do
-    skip "can't tell why requests doesn't appear as accepted.
-      Maybe fixture state refuses to change after call?"
+    skip "can't tell why requests doesn't appear as accepted."
 
-    request = friend_requests(:one)
+    assert @request.pending?, 'default status should be pending'
 
-    assert request.pending?, 'default status should be pending'
+    post accept_friend_request_url(@request)
 
-    post accept_friend_request_url(request)
-
-    assert request.accepted?, 'request should have been accepted'
+    assert @request.accepted?, 'request should have been accepted'
   end
 end

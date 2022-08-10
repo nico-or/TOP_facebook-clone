d04574f0c69e49f33f849dc1b2471a2488b304b1
Nicolás Órdenes <nicordenes@gmail.com>
2022-08-10 13:02:20 -0400

    Move sorting scopes to AppRec::Base
    To avoid duplicating the newest_first and oldest_first scopes in the
    Comment class, they were moved to the ApplicationRecord::Base class.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  scope :newest_first, -> { order(updated_at: :desc) }
+  scope :oldest_first, -> { order(updated_at: :asc) }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,4 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liking_users, through: :likes, source: :user
-
-  scope :newest_first, -> { order(updated_at: :desc) }
-  scope :oldest_first, -> { order(updated_at: :asc) }
 end

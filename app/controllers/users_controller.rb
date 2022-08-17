7406762bd563205bbe5e674e07cf545784c68b76
Nicolás Órdenes <nicordenes@gmail.com>
2022-08-17 14:04:21 -0400

    Refactor users controller
    Add before_action filter for setting the user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show update]
+
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def profile
@@ -13,7 +14,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "User was successfully updated"
       redirect_to @user
@@ -24,6 +24,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:description)

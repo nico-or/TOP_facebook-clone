d6bc573ea5ccab6ddf2e58a7efd4fc9f4e4924a9
Nicolás Órdenes <nicordenes@gmail.com>
2022-09-27 14:57:51 -0300

    refactor(seed): simplify user creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,29 @@
 # frozen_string_literal: true
 
 # helper methods
-def lorem_paragraph
-  Faker::Lorem.paragraph(sentence_count: 4, supplemental: true, random_sentences_to_add: 12)
-end
-
-def lorem_description(n = 3)
-  n.times.map do
-    lorem_paragraph
-  end.join("\n\n")
-end
-
 
 # Default User
-User.create(username: 'UserName', email: 'user@example.com', password: '123456', description: lorem_description)
+User.create(
+  username: "username",
+  email: "user@example.com",
+  password: "123456",
+  description: "default description.",
+)
 
 # Populate with Users
-9.times do
-  user = Faker::Internet.user(:username, :safe_email, :password)
-
-  # rename :safe_email key
-  mask = { safe_email: :email }
-  new_user = user.each.with_object({}) { |(k, v), memo| memo[mask[k] || k] = v }
-
-  new_user[:description] = lorem_description
-
-  User.create(new_user)
+9.times do |n|
+  User.create(
+    username: "user-#{n}",
+    email: "user-#{n}@example.com",
+    password: "password",
+    description: "default description.",
+  )
 end
 
 # Populate with Posts
 users = User.all.records
 5.times do
   user = users.sample
-  text = lorem_description
+  text = "lorem ipsum..."
   user.posts.create(body: text)
 end
